08db6c1318c572e4f28cba46b2780d684434bf40
Sergey <sa6702@gmail.com>
2023-04-11 07:25:37 +0100

    update

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,29 @@
+# from manual
 from pydantic import BaseModel
+
+
+mock_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+def mock_hash_password(password: str):
+    return "fakehashed" + password
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class User(BaseModel):
@@ -6,3 +31,7 @@ class User(BaseModel):
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
--- app/routers/manual/user_manual.py
+++ app/routers/manual/user_manual.py
@@ -1,14 +1,25 @@
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import (
+    Depends, APIRouter, HTTPException,
+    # status
+    )
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from app.models.user_model import User
-
+from app.models.user_model import (
+    User, UserInDB, mock_users_db,
+    mock_hash_password
+    )
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
 
 def fake_decode_token(token):
@@ -23,6 +34,29 @@ async def get_current_user(
         token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = mock_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = mock_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
 @router.get("/me")
